de05e4918e736fb4a12e63cad93e21c9ec0fe0fc
Kairo Amorim <kairo.amorim2001@gmail.com>
2022-07-13 20:15:32 -0300

    Refac snake_case

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:team_dex/ui/screen/my-app.dart';
+import 'package:team_dex/ui/screen/my_app.dart';
 
 void main() {
   runApp(const MyApp());
--- lib/ui/screen/my_app.dart
+++ lib/ui/screen/my_app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:team_dex/ui/screen/home-screen.dart';
+import 'package:team_dex/ui/screen/home_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
