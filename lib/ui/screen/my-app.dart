5fba83f447f3b9b05753eb3b66763ed6baf0735c
RafaelBicario <rafabicario@outlook.com>
2022-07-13 20:11:03 -0300

    Screens
    Remoção da MyApp e HomePage do Main

---
+++ lib/ui/screen/my-app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:team_dex/ui/screen/home-screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TeamDex',
+      theme: ThemeData(primarySwatch: Colors.blue, brightness: Brightness.dark),
+      home: const HomePage(),
+    );
+  }
+}
