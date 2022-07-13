0b79162412ffb766f5cb6a7675d00e425853b9af
Kairo Amorim <kairo.amorim2001@gmail.com>
2022-07-13 20:24:09 -0300

    Novas classes para conexão com APIs externas

---
+++ lib/core/controller/dio_config.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+import 'package:dio/adapter.dart';
+import 'package:dio/dio.dart';
+
+import './utils/constants.dart';
+
+class DioConfig {
+  /// Aqui é onde a dependência do Dio é configurada para comunicação com as APIs externas
+  static Future<Dio> builderConfig({findToken = false}) async {
+    var dio = Dio(BaseOptions(
+        baseUrl: POKE_API_IP,
+        receiveDataWhenStatusError: true,
+        connectTimeout: 30 * 1000,
+        receiveTimeout: 30 * 1000));
+    if (Platform.isAndroid) {
+      (dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate = (client) {
+        client.badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+        return client;
+      };
+    }
+    return dio;
+  }
+}
---
+++ lib/core/controller/utils/constants.dart
@@ -0,0 +1,1 @@
+const String POKE_API_IP = "https://pokeapi.co/api/v2";
